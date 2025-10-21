b75ca37cff3231b850dd6389fa564898a2fdc358
Ethan Lu <ethanluthewolf@gmail.com>
2025-10-21 10:25:59 -0700

    Commit

--- notes-5-recursion.py
+++ notes-5-recursion.py
@@ -15,6 +15,7 @@ t.goto(0, -200)
 t.color("brown")
 t.width(10)
 t.shape("arrow")         # leaf shape
+t.speed(100000000)
 
 def draw_tree(level: int, branch_length: float):
     """Draw a tree recursively at a given level
@@ -42,6 +43,35 @@ def draw_tree(level: int, branch_length: float):
         t.stamp()
         t.color("brown")
 
-draw_tree(6, 200)
+def draw_complicated_tree(level: int, branch_length: float):
+    """Draw a tree recursively at a given level
+    level - the levels of branches
+    branch_length - length of branch in pixels
+    """
+    t.pendown()
+
+    # If the level is greater than zero
+    if level > 0:
+        # 1. Move forward branch_length
+        t.forward(branch_length)
+        # 2. Turn left and draw a "smaller tree"
+        t.left(55)
+        draw_complicated_tree(level - 1, branch_length * 0.8)
+        # 4. Turn middle and draw a "smaller tree"
+        t.right(55)
+        draw_complicated_tree(level - 1, branch_length * 0.8)
+        # 5. Turn right and draw a "smaller tree"
+        t.right(55)
+        draw_complicated_tree(level - 1, branch_length * 0.8)
+        # 6. Return to the beginning
+        t.left(55)
+        t.backward(branch_length)
+    else:
+        # create a leaf
+        t.color("green")
+        t.stamp()
+        t.color("brown")
+
+draw_complicated_tree(4, 200)
 
 wn.exitonclick()
