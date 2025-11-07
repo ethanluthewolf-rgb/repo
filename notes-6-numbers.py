4439e80f7a54a50da7c634525ccd91107fc2e6b5
Ethan Lu <ethanluthewolf@gmail.com>
2025-11-07 10:11:08 -0800

    notes

--- notes-6-numbers.py
+++ notes-6-numbers.py
@@ -7,12 +7,38 @@
 # bubble tea place around us
 
 # Version 1
-# Show all the bbt choices
-# Ask the user for their choice
-# Add their vote to a running
-# tally
-# Give some raw scores
-# Give score as a percentage
+def vote_listed_choices():
+    """Display all voting choices
+    5 users vote for their choice
+    Results will be printed"""
+    CHOICES = [
+        "A. CoCo",
+        "B. Chatime",
+        "C. BUBBLE WAFFEL",
+        "D. gong cha"
+    ]
+
+    # Buckets to hold all the votes
+    coco = 0
+    chatime = 0
+    bubble_waffel = 0
+    gong_cha = 0
+
+    # Show all the bbt choices
+    print("Vote for your favourite from the list.")
+    print("Give the letter of your choice.")
+    for choice in CHOICES:
+        print(choice)
+    # Ask the user for their choice
+    vote = input("Your vote: ").lower().strip(",.?! ")
+    # Add their vote to a running
+    # tally
+    if vote == "a":
+        coco = coco + 1  # incrementation
+    elif vote == "b":
+        chatime += 1
+    # Give some raw scores
+    # Give score as a percentage
 
 # Version 2
 # Ask the user what their fave
@@ -21,3 +47,9 @@
 # tally
 # Give the raw score
 # Give score as percentage
+
+def main():
+    vote_listed_choices()
+
+if __name__ == "__main__":
+    main()
