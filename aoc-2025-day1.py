b70ced06c6fd2af01b5748d6bab6e17543d870a5
Ethan Lu <ethanluthewolf@gmail.com>
2025-12-01 11:44:13 -0800

    mmmaswasdjklwjakljsdjkw akjljskd jaj

---
+++ aoc-2025-day1.py
@@ -0,0 +1,18 @@
+# Advent Of Code
+# Author: Ethan Lu
+# December 1, 2025
+
+def part_one():
+    # read every line in the instructions
+    with open("data/aoc-2025-day1.txt") as f:
+        count = 0
+        for line in f:
+            pass
+    # instruction exampe "R10" -> right 10
+    # if we go RIGHT -> add
+    # if we go left -> subtract
+    # if we land on zero keep track of it
+    # print how many times we landed on zero
+
+if __name__== "__main__":
+    part_one()
